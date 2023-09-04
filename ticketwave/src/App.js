21dd8aa2a2ccbf3737c41f297d601e35c00fa276
AubBantolo <jhuliet14@gmail.com>
2023-09-04 17:03:51 +0800

    refactor: removed unwanted codes

--- ticketwave/src/App.js
+++ ticketwave/src/App.js
@@ -1,17 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import Movies from './components/Movies';
 
 function App() {
-  const LOCAL_BASE_URL = "http://localhost:8080/";
-
-  const baseURLByEnv = process.env.REACT_APP_BASE_URL || LOCAL_BASE_URL;
- 
-  const load = () => {
-    const targetURL = 'Movies.js';
-
-    window.location.href = targetURL;
-  }
 
   return (
     <div className="App">
