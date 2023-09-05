a7f4e6559330e2435664f362016ff161fa903587
Arianne <chiescake11@gmail.com>
2023-09-05 13:56:28 +0800

    refactor: remove links; remove unused codes

--- ticketwave/src/App.js
+++ ticketwave/src/App.js
@@ -7,22 +7,11 @@ function App() {
 
   return (
     <div className="App">
-     
+
       <nav>
         <head>
         </head>
-        {/* <Movies> </Movies> */}
         <SearchMovie />
-        <ul>
-          <li><a href={"/"}>Home</a></li>
-          <li><a href={"/booking"}>Booking</a></li>
-          <li><a href={"/payment"}>Payment</a></li>
-          <li><a href={"/createAccount"}>Cerate Account</a></li>
-          <li><a href={"/login"}>Login Account</a></li>
-          <li><a href={"/movieDetail"}>Movie Detail</a></li>
-          <li><a href={"/about"}>About</a></li>
-          <li><a href={"/error"}>Error</a></li>
-        </ul>
       </nav>
       <Outlet />
     </div>
--- ticketwave/src/components/SearchBar.js
+++ ticketwave/src/components/SearchBar.js
@@ -6,8 +6,6 @@ import { resetMovie } from '../components/movieSlice';
 import * as dashboardApi from "../api/dashboardApi";
 import '../css/SearchBar.css';
 
-
-const { Search } = Input;
 const { Option } = AutoComplete;
 
 
@@ -30,49 +28,29 @@ export const SearchBar = () => {
         setFilteredMovies(movieList.filter(movies => movies.movieTitle.toLowerCase().includes(value.toLowerCase())));
     }
 
-    const searchMovies = async (movieTitle) => {
-        // try {
-        //   const response = await dashboardApi.searchMovieByTitle(movieTitle);
-      
-        //   setFilteredMovies(response.data);
-        //   console.log(response.data);
-        // } catch (error) {
-        //   console.error('Error searching movies:', error);
-        // }
-      };
-
     return (
         <div>
-        {
-            movieList.map((items) =>
-            <span key={items.id}>
-                {/* {items.movieTitle} */}
-            </span>
-            
-        )}
-            
-        <div className="searchBar">
-            <AutoComplete
-                style={{ width: 300 }}
-                placeholder="Search Movie"
-                value={input}
-                onChange={(value) => handleChange(value)}
-                onSelect={(value) => searchMovies(value)}
-            >
-                {filteredMovies.map((movie) => (
-                    <Option key={movie.id} value={movie.movieTitle}>
-                        {movie.movieTitle}
-                    </Option>
-                ))}
+            <div className="searchBar">
+                <AutoComplete
+                    style={{ width: 300 }}
+                    placeholder="Search Movie"
+                    value={input}
+                    onChange={(value) => handleChange(value)}
+                >
+                    {filteredMovies.map((movie) => (
+                        <Option key={movie.id} value={movie.movieTitle}>
+                            {movie.movieTitle}
+                        </Option>
+                    ))}
 
-      
-            </AutoComplete>
-            <button className="submitButton" type="submit">
-            <SearchOutlined className="iconSearch"/>
-            </button>
-        
+
+                </AutoComplete>
+                <button className="submitButton" type="submit">
+                    <SearchOutlined className="iconSearch" />
+                </button>
+
+            </div>
+
         </div>
-
-    </div>
     )
 }
