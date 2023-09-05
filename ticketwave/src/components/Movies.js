92cfb258f858d6fd784f8aef5c85fbdbfdb67e92
AubBantolo <jhuliet14@gmail.com>
2023-09-05 20:32:33 +0800

    feat: added movies and css

--- ticketwave/src/components/Movies.js
+++ ticketwave/src/components/Movies.js
@@ -1,33 +1,137 @@
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { resetMovie } from '../components/movieSlice';
 import * as dashboardApi from "../api/dashboardApi"; 
+import '../css/Movies.css';
+import { Col, Row} from 'antd';
+import { NavLink } from "react-router-dom";
+import Carousel from "react-multi-carousel";
+import { CaretRightOutlined } from '@ant-design/icons';
+import "react-multi-carousel/lib/styles.css";
+import '../App.css';
 
 const Movies = () => {
     const dispatch = useDispatch();
 
     const movieList = useSelector((state) => state.movieSlice.movieSlice);
 
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 5,
+          slidesToSlide: 2 
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 768 },
+          items: 3,
+          slidesToSlide: 3 
+        },
+        mobile: {
+          breakpoint: { max: 767, min: 464 },
+          items: 2,
+          slidesToSlide: 1 
+        }
+      };
+
     useEffect(() => {
         const fetchData = async () => {
-            const response = await dashboardApi.getAllMovies();
+            const response = await dashboardApi.getAllMovies(); 
             dispatch(resetMovie(response.data))
+            console.log(response.data);
         }
         fetchData();
     }, []);
 
     return (
-        <div>
-            {
-                movieList.map((items) =>
-                <span key={items.id}>
-                    text={items.text}
-                    </span>
-                
-            )}
+        <div className="parent">
+
+            <Carousel
+                responsive={responsive}
+                autoPlay={true}
+                swipeable={false}
+                draggable={false}
+                infinite={true}
+                partialVisible={false}
+                arrows={false}
+                dotListClass="custom-dot-list-style"
+            >
+                {movieList.map((item, index) => (
+                <div key={index} className="slider">
+                    <img
+                    className="movie-image"
+                    src={item.imageUrl}
+                    alt={item.title}
+                    style={{
+                        height: '500px',
+                        width: '380px'
+                    }}
+                />
+            </div>
+            ))}
+            </Carousel>  
+
+            <h2 id="movie-reco-title">Movie Recommendation
+            <button> <CaretRightOutlined className="hello"/></button>
+            </h2>
+          
+
+            <div className="movies-reco">
+                <Carousel
+                responsive={responsive}
+                autoPlay={false}
+                swipeable={true}
+                draggable={true}
+                infinite={true}
+                partialVisible={false}
+                arrows={true}
+                dotListClass="custom-dot-list-style"
+                >
+                {movieList.map((item, index) => (
+                <div key={index} className="slider">
+                    <img
+                    className="movie-image"
+                    src={item.imageUrl}
+                    alt={item.title}
+                    style={{
+                        height: '450px',
+                        width: '350px'
+                    }}
+                    />
+                </div>
+                ))}
+                </Carousel>  
+            </div>
+
+            <h2 id="movie-avail-title">Available Movies
+            <button> <CaretRightOutlined /></button>
+            </h2>
             
-            
-        </div>
+            <div className="movies-avail">
+                <Carousel
+                responsive={responsive}
+                autoPlay={false}
+                swipeable={true}
+                draggable={true}
+                infinite={true}
+                partialVisible={false}
+                dotListClass="custom-dot-list-style"
+                >
+                {movieList.map((item, index) => (
+                <div key={index} className="slider">
+                    <img
+                    className="movie-image"
+                    src={item.imageUrl}
+                    alt={item.title}
+                    style={{
+                        height: '450px',
+                        width: '350px'
+                    }}
+                    />
+                </div>
+                ))}
+                </Carousel>  
+            </div>
+      </div>
     )
 };
 
