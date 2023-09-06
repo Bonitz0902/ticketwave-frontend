4e35270b100928ebadccc1028dcf1d947d6447b4
elainedelosreyes1971 <140990897+elainedelosreyes1971@users.noreply.github.com>
2023-09-06 19:57:43 +0800

    refactor: updated based on code review suggestions

--- ticketwave/src/pages/MovieDetailPage.js
+++ ticketwave/src/pages/MovieDetailPage.js
@@ -8,20 +8,25 @@ export const MovieDetailPage = () => {
     const movieList = useSelector(state => state.movieSlice.movieSlice);
     const selectedMovie = useSelector(state => state.movieSlice.selectedMovie);
 
+    const gotoBookingPage = () => {
+         navigate('/booking');
+    }
+
     return (
-        <div>
+        <div className={"movieContainer"}>
             <div className={"movieDetails"} >
+                <ArrowLeftOutlined onClick={goBack}/>
                 {
                     movieList.filter(movie => selectedMovie === movie.id).map((item) =>
                         <div key={item.id}>
-                        <h1>Movie Details</h1>
-                        <center><Image preview={false} width={250} src={item.imageUrl}/>
+                        <h1>{item.movieTitle}</h1>
+                        <center><Image preview={false} src={item.imageUrl} className={"image"}/>
                         <h2>{item.movieTitle}</h2>
                         <p className={"movieDescription"}>{item.description}</p>
                         <p>Rating: {item.rating}/10<StarFilled className={"ratingIcon"}/>  712k</p>
                         <p>Genre: {item.genre}</p>
                         <p>Director: {item.director}</p>
-                        <Button type={"primary"} className={"bookNowBtn"}> Book Now </Button></center>
+                        <Button type={"primary"} style={{borderRadius: "20px"}} className={"bookNowBtn"} onClick={gotoBookingPage} size={"large"}> Book Now </Button></center>
                         </div>
                     )}
             </div>
