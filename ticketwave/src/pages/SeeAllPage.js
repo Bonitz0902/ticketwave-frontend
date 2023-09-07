ee0f243df92fb939c23eafbf065cd5faca88e5f6
Arianne <chiescake11@gmail.com>
2023-09-07 20:51:05 +0800

    refactor: modify the navigation to go back

--- ticketwave/src/pages/SeeAllPage.js
+++ ticketwave/src/pages/SeeAllPage.js
@@ -1,10 +1,10 @@
 import SearchMovie from '../components/SearchMovie';
-import { useSelector,  useDispatch} from 'react-redux';
-import { setSelectedMovie } from '../components/movieSlice';
+import {useSelector, useDispatch} from 'react-redux';
+import {setSelectedMovie} from '../components/movieSlice';
 import '../css/SeeAllPage.css';
 import Slider from "react-slick";
-import { ArrowLeftOutlined } from '@ant-design/icons';
-import { useNavigate } from "react-router-dom";
+import {ArrowLeftOutlined} from '@ant-design/icons';
+import {useNavigate} from "react-router-dom";
 
 export const SeeAllPage = () => {
     const movieList = useSelector((state) => state.movieSlice.movieSlice);
@@ -12,25 +12,25 @@ export const SeeAllPage = () => {
     const dispatch = useDispatch();
 
     const settings = {
-        dots: false, 
+        dots: false,
         infinite: true,
-        speed: 500, 
+        speed: 500,
         slidesPerRow: 3,
         rows: 8,
 
         esponsive: [
-         {
-             breakpoint: 768,
-             settings: {
-                 slidesPerRow: 2,
-             },
-         },
-         {
-             breakpoint: 576, 
-             settings: {
-                 slidesPerRow: 1,
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesPerRow: 2,
+                },
             },
-        },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesPerRow: 1,
+                },
+            },
         ],
     };
 
@@ -41,26 +41,26 @@ export const SeeAllPage = () => {
 
 
     const goBack = () => {
-       navigate("/");
-    }    
+        navigate(-1);
+    }
 
     return (
         <div className='parent'>
-           
+
             <nav>
-                <SearchMovie/>
+                <SearchMovie />
             </nav>
-            <ArrowLeftOutlined className="goBack" onClick={() => goBack()}/>
+            <ArrowLeftOutlined className="goBack" onClick={() => goBack()} />
             <div className="movies-reco">
-            <Slider {...settings}>
-              {movieList.map((item, index) => (
-                   <div key={index} className="card_content">
-                        <div key={index} className="card-pad">
-                            <img src={item.imageUrl} onClick={() => selectMovie(item.id)}/>
+                <Slider {...settings}>
+                    {movieList.map((item, index) => (
+                        <div key={index} className="card_content">
+                            <div key={index} className="card-pad">
+                                <img src={item.imageUrl} onClick={() => selectMovie(item.id)} />
+                            </div>
                         </div>
-                  </div>
-              ))}
-              </Slider>
+                    ))}
+                </Slider>
             </div>
         </div>
     )
